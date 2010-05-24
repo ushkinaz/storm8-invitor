e5757456322757d645b3f109b566854496372913
Dmitry Sidorenko <rustydragon@gmail.com>
2010-05-24 17:49:41 +0400

    Fixed xml format

--- src/main/java/net/ushkinaz/storm8/domain/Game.java
+++ src/main/java/net/ushkinaz/storm8/domain/Game.java
@@ -11,13 +11,13 @@ import java.util.Map;
 
 /**
  * @author Dmitry Sidorenko
- * @date May 24, 2010
  */
 public class Game implements XMLSerializable {
+    @SuppressWarnings({"UnusedDeclaration"})
     private static final org.apache.commons.logging.Log LOGGER = org.apache.commons.logging.LogFactory.getLog(Game.class);
 
+    private String name;
     private String domain;
-    private String url;
     private String clan_uri;
     private Map<String, String> cookies;
     private List<Topic> topics;
@@ -39,24 +39,16 @@ public class Game implements XMLSerializable {
         return domain;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void setDomain(String domain) {
         this.domain = domain;
     }
 
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
     public List<Topic> getTopics() {
         return topics;
-    }
-
-    public String getClan_uri() {
-        return clan_uri;
     }
 
     public void setClan_uri(String clan_uri) {
@@ -75,29 +67,33 @@ public class Game implements XMLSerializable {
         Game game = (Game) o;
 
         if (clan_uri != null ? !clan_uri.equals(game.clan_uri) : game.clan_uri != null) return false;
-        if (!domain.equals(game.domain)) return false;
-        if (url != null ? !url.equals(game.url) : game.url != null) return false;
+        if (domain != null ? !domain.equals(game.domain) : game.domain != null) return false;
+        if (name != null ? !name.equals(game.name) : game.name != null) return false;
 
         return true;
     }
 
     @Override
     public int hashCode() {
-        return domain.hashCode();
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (domain != null ? domain.hashCode() : 0);
+        result = 31 * result + (clan_uri != null ? clan_uri.hashCode() : 0);
+        return result;
     }
 
+    @SuppressWarnings({"UnusedDeclaration"})
     protected static final XMLFormat<Game> GAME_XML = new XMLFormat<Game>(Game.class) {
         public void write(Game g, XMLFormat.OutputElement xml) throws XMLStreamException {
-            xml.setAttribute("domain", g.domain);
-            xml.add(g.url, "url");
+            xml.setAttribute("name", g.name);
+            xml.add(g.domain, "domain");
             xml.add(g.clan_uri, "clan_uri");
             xml.add(g.cookies, "cookies");
             xml.add(g.topics, "topics");
         }
 
         public void read(InputElement xml, Game g) throws XMLStreamException {
-            g.domain = xml.getAttribute("domain", "");
-            g.url = xml.get("url");
+            g.name = xml.getAttribute("name", "");
+            g.domain = xml.get("domain");
             g.clan_uri = xml.get("clan_uri");
             g.cookies = xml.get("cookies");
             g.topics = xml.get("topics");
--- src/test/java/net/ushkinaz/storm8/domain/GameTest.java
+++ src/test/java/net/ushkinaz/storm8/domain/GameTest.java
@@ -30,8 +30,8 @@ public class GameTest {
         binding = xmlBinderFactory.get();
 
         game = new Game();
-        game.setDomain("Ninja");
-        game.setUrl("http://nl.storm8.com");
+        game.setName("Ninja");
+        game.setDomain("nl.storm8.com");
         game.setClan_uri("/group.php");
         game.getCookies().put("ask", "1ec7c54864b2d968f89aa6453b067a9d4bff8a2f");
         game.getCookies().put("st", "2792593%2Cc2e17ffa909fd6d4a6b1bbe219fed884ffddb425%2C1274554582%2C12%2C%2Ca1.54%2C14%2C4%2C10003%2C2010-05-22+11%3A56%3A22%2C%2Cv1_1274554582_5e4680101c1b52f0674b973b4a03b1aaf3043356");
